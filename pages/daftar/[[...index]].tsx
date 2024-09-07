9b41f92390e3d5aa88d4b15927a9dfad0e3024fd
MhinHub <iskandarm20d@student.unhas.ac.id>
2024-09-07 15:53:35 +0800

    chore: update paths for auth

--- pages/daftar/[[...index]].tsx
+++ pages/daftar/[[...index]].tsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs';
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <section className="w-screen h-fit flex justify-center items-center">
+      <SignUp />
+    </section>
+  );
 }
