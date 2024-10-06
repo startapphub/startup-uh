00aec6d61b007189321e9b10e9cbd33bd8ac1557
MhinHub <iskandarm20d@student.unhas.ac.id>
2024-10-06 12:44:45 +0800

    chore: ignore eslint during build

--- next.config.js
+++ next.config.js
@@ -8,11 +8,14 @@ const withPWA = require('next-pwa')({
 module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
-		remotePatterns: [
-			{
-				hostname: "**",
-			},
-		],
-	},
+    remotePatterns: [
+      {
+        hostname: '**',
+      },
+    ],
+  },
 });
